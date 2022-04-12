6478ad4d5baa12972e9d99299458ee4b566876aa
efs0-cod3 <eflearnng@gmail.com>
2022-04-12 12:21:17 -0400

    feedback

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
 let btn = document.querySelector('.btn').addEventListener('click', searchAirport)
+let inputStyle = document.querySelector('.input')
 let info = document.querySelector('.infoHere')
-let item,lInfo,met;
+let lInfo, met;
 let list = document.querySelector('.listHere')
 let metarInfo = document.querySelector('.metar')
-function searchAirport () {
-list.classList.add('active')
-metarInfo.classList.add('active')
-	
-	let input = document.querySelector('.input').value
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Host': 'airport-info.p.rapidapi.com',
-			'X-RapidAPI-Key': '4d6ecc08fbmsh4b36dbb74d831efp15fe8ajsn9f92fe369271'
-		}
-	};
-	
-	fetch(`https://airport-info.p.rapidapi.com/airport?icao=${input}`, options)
-		.then(res => res.json())
-		.then(data => {
+let wText = document.querySelector('.text')
+
+function removeWarning() {
+	inputStyle.classList.remove('input--warning')
+	wText.classList.remove('text--warning')
+
+}
+
+function searchAirport() {
+	list.classList.add('active')
+	metarInfo.classList.add('active')
+
+	let input = document.querySelector('.input').value;
+	if (input.length != 4) {
+		inputStyle.classList.add('input--warning')
+		wText.classList.add('text--warning')
+		info.style.display = 'none'
+		setTimeout(removeWarning, 2000);
+	} else {
+
+		const options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Host': 'airport-info.p.rapidapi.com',
+				'X-RapidAPI-Key': '4d6ecc08fbmsh4b36dbb74d831efp15fe8ajsn9f92fe369271'
+			}
+		};
+
+		fetch(`https://airport-info.p.rapidapi.com/airport?icao=${input}`, options)
+			.then(res => res.json())
+			.then(data => {
 				lInfo = `
 				<li><span>IATA code:</span> ${data.iata}</li>
 				<li><span>Name:</span> ${data.name}</li>
@@ -29,9 +45,11 @@ metarInfo.classList.add('active')
 				<li><span>Handler's site:</span><br> <a href='${data.website}'>${data.website}</a></li>			
 				`
 
-					list.innerHTML = lInfo
-		})
-		.catch(err => console.error(err));
+				list.innerHTML = lInfo
+
+
+			})
+			.catch(err => console.error(err));
 
 
 		const metar = {
@@ -41,10 +59,13 @@ metarInfo.classList.add('active')
 				'X-RapidAPI-Key': '4d6ecc08fbmsh4b36dbb74d831efp15fe8ajsn9f92fe369271'
 			}
 		};
-		
+
 		fetch(`https://aerisweather1.p.rapidapi.com/observations/${input}`, metar)
 			.then(response => response.json())
-			.then(data => {			
+			.then(data => {
+				if (input == '') {
+					met = `Insert ICAO code`
+				}
 				met = `
 				<li class='title'> ${data.response.dataSource}</li>
 				<li><span>RAW:</span><br> ${data.response.raw}</li>
@@ -55,6 +76,5 @@ metarInfo.classList.add('active')
 				metarInfo.innerHTML = met
 			})
 			.catch(err => console.error(err));
+	}
 }
-
-
